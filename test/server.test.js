b0ec3b3676c9b1f88bf565e6b40f95dc97452093
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2018-04-27 09:56:10 -0600

    added travis yml file and test folder

---
+++ test/server.test.js
@@ -0,0 +1,22 @@
+'use strict';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+// const app = require('../server');
+chai.use(chaiHttp);
+
+describe('Reality check', function(){
+  it('should return true', function(){
+    expect(true).to.be.true;
+  });
+});
+
+// describe('Get Notes', function () {
+//   it('should retrieve all the notes from the database', function (){
+//     return chai.request(app)
+//       .get('/api/notes')
+//       .then(function (res){
+//         expect(res).to.exist;
+//       });
+//   });
+// });
